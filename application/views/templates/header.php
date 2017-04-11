d2e06f75aadbec61cb3d07956de8cb7f2559c96b
Zámbó Sándor Dávid <david.a.zambo@gmail.com>
2017-04-11 11:41:14 +0200

    Index page almost ready, starting the form

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+  <link href="http://bootswatch.com/simplex/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  <link href="<?php echo base_url(); ?>assets/css/styles.css" rel="stylesheet" type="text/css"/>
+  <title></title>
+</head>
+<body>
+  <div class="container">
