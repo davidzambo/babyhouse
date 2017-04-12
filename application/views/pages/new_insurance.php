419d4096a307fde21dea97445f799882856fc210
Zámbó Sándor Dávid <david.a.zambo@gmail.com>
2017-04-12 07:15:22 +0200

    New insurance form is on the halfway. Leap year is not checked yet!

--- application/views/pages/new_insurance.php
+++ application/views/pages/new_insurance.php
@@ -4,12 +4,12 @@
   <legend>Új biztosítási ajánlat rögzítése</legend>
   <div class="form-group">
 
-    <label for="person_title" class="col-lg-4 control-label">Biztosított <strong>neme</strong>:</label>
+    <label for="person_title" class="col-lg-4 control-label">Biztosított <strong>megszólítása</strong>:</label>
     <div class="col-lg-3">
       <select class="form-control" id="person_title">
         <option value="default">Kérem válasszon!</option>
-        <option value="no">nő</option>
-        <option value="ferfi">férfi</option>
+        <option value="holgyem">Hölgyem</option>
+        <option value="uram">Uram</option>
       </select>
     </div>
     <br/>
@@ -20,7 +20,7 @@
       <div class="col-lg-4">
         <input type="text" class="form-control" id="first_name" placeholder="Keresztnév">
       </div>
-    </div>
+  </div>
 
   <div class="form-group">
     <label for="last_name" class="col-lg-4 control-label">Biztosított <strong>vezetékneve</strong>:</label>
@@ -31,14 +31,35 @@
 
   <div class="form-group">
     <label for="birth_date" class="col-lg-4 control-label">Biztosított <strong>születési ideje</strong>:</label>
-      <div class="col-lg-1">
-        <input type="text" class="form-control" id="birth_year" placeholder="Év">
+      <div class="col-lg-2">
+        <select class="form-control" id="birth_year">
+          <option value="default">év</option>
+          <?php
+            for ($i=date("Y")-10 ; $i>date("Y")-91 ; $i--){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
       </div>
-      <div class="col-lg-1">
-        <input type="text" class="form-control" id="birth_month" placeholder="Hónap">
+      <div class="col-lg-2">
+        <select class="form-control" id="birth_month">
+          <option value="default">hónap</option>
+          <?php
+            for ($i=1 ; $i<=12 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
       </div>
-      <div class="col-lg-1">
-        <input type="text" class="form-control" id="birth_day" placeholder="Nap">
+      <div class="col-lg-2">
+        <select class="form-control" id="birth_day">
+          <option value="default">nap</option>
+          <?php
+            for ($i=1 ; $i<=31 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
       </div>
   </div>
 
@@ -80,8 +101,83 @@
           <option value="szlovenia">Szlovénia</option>
         </select>
       </div>
-
   </div>
+
+  <div class="form-group">
+    <label for="treatment_date" class="col-lg-4 control-label">A <strong>kezelési terv dátuma</strong>:</label>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_year">
+          <option value="default">év</option>
+          <?php
+            for ($i=date("Y") ; $i>date("Y")-5 ; $i--){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_month">
+          <option value="default">hónap</option>
+          <?php
+            for ($i=1 ; $i<=12 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_day">
+          <option value="default">nap</option>
+          <?php
+            for ($i=1 ; $i<=31 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+  </div>
+
+  <div class="form-group">
+    <label for="treatment_id" class="col-lg-4 control-label">A kezelési terv <strong>azonosító száma</strong>:</label>
+      <div class="col-lg-4">
+        <input type="text" class="form-control" id="treatment_id" placeholder="Azonosítós szám">
+      </div>
+  </div>
+
+  <div class="form-group">
+    <label for="treatment_finish_date" class="col-lg-4 control-label">A kezelés <strong>befejezésének dátuma</strong>:</label>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_finish_year">
+          <option value="default">év</option>
+          <?php
+            for ($i=date("Y") ; $i>date("Y")-5 ; $i--){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_finish_month">
+          <option value="default">hónap</option>
+          <?php
+            for ($i=1 ; $i<=12 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+      <div class="col-lg-2">
+        <select class="form-control" id="treatment_finish_day">
+          <option value="default">nap</option>
+          <?php
+            for ($i=1 ; $i<=31 ; $i++){
+              echo "<option value='".$i."'>".$i."</option>\n";
+            }
+           ?>
+        </select>
+      </div>
+  </div>
+
   </div>
 </fieldset>
 </form>
